418e854a473b6a7b4dd814c19d3869062c5a84a2
now <lynx.ky@outlook.com>
2025-05-25 00:48:52 -0300

    Delete src directory

--- src/sendContent.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from "vscode";
-import { getWebSocketClients } from "./websocket";
-
-export default (fileName: string, content: string) => {
-    const webSocketsClients = getWebSocketClients();
-
-    webSocketsClients.forEach(wsClient => wsClient.send(JSON.stringify({ fileName, content })));
-    vscode.window.showInformationMessage(`Sent to ${webSocketsClients.length} WebSockets`);
-};
