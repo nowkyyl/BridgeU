541ae35528c901b367e1f06875eb394848b5f7b6
now <lynx.ky@outlook.com>
2025-05-25 00:35:59 -0300

    Update extension.test.ts

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -9,26 +9,4 @@ suite('BridgeU Extension Test Suite', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
 	});
-
-	test('WebSocket connection and send message', function(done) {
-		this.timeout(50000);
-
-		const ws = new WebSocket('ws://localhost:8080/');
-
-		ws.on('open', () => {
-			const message = JSON.stringify({
-				fileName: "test.lua",
-				content: 'print("Hello from test")'
-			});
-			ws.send(`Output:${message}`);
-		});
-
-		ws.on('message', (data) => {
-			done();
-		});
-
-		ws.on('error', (err) => {
-			done(err);
-		});
-	});
 });
